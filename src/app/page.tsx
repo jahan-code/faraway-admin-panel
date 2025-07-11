03d8b9bca1514a0edfe4a0bd1289010c13b05c01
Muhammad Noman <jawad.bdo99@gmail.com>
2025-07-11 12:55:37 +0500

    push code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import SailAwayCard from "@/components/SailAwayCard";
 export default function Home() {
   return (
     <div className="max-w-7xl py-2 lg:py-5 mx-auto sm:px-4 flex items-center justify-center h-screen">
-      <div className="flex gap-12 xl:gap-20">
-        <div className="w-full lg:w-[980px] xl:w-[1200px] sm:mx-auto px-4 space-y-6 flex flex-col justify-between">
+      <div className="flex gap-10 xl:gap-15 2xl:gap-20">
+        <div className="w-full lg:w-[955px] xl:w-[1150px] sm:mx-auto px-4 space-y-6 flex flex-col justify-between">
           <div className="flex-grow flex items-center">
             <div
               className="bg-white w-full px-6 space-y-8"
--- src/components/SailAwayCard/index.tsx
+++ src/components/SailAwayCard/index.tsx
@@ -5,7 +5,7 @@ const SailAwayCard: React.FC = () => {
 
   return (
     <div className="relative w-full">
-      <Image src="/images/sailaway.svg" alt="img" width={0} height={917} className="w-full" />
+      <Image src="/images/sailaway.svg" alt="img" width={0} height={917} className="w-full h-[calc(100vh-50px)]" />
     </div>
   );
 };
