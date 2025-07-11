dc34f8bb78563d8b4c7a28ed8d56173573a5a21d
Muhammad Noman <jawad.bdo99@gmail.com>
2025-07-11 12:58:27 +0500

    update code

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -11,8 +11,8 @@ const AuthLayout: React.FC<Props> = ({ children }) => {
   
   return (
     <div className="max-w-7xl py-2 lg:py-5 mx-auto sm:px-4 flex items-center justify-center h-screen">
-      <div className="flex gap-10 xl:gap-20">
-        <div className="w-full lg:w-[980px] xl:w-[1200px] sm:mx-auto px-4 space-y-6 flex flex-col justify-between">
+      <div className="flex gap-10 xl:gap-15 2xl:gap-20">
+        <div className="w-full lg:w-[955px] xl:w-[1150px] sm:mx-auto px-4 space-y-6 flex flex-col justify-between">
           <div className="flex-grow flex items-center">
             <div className="bg-white w-full px-6 space-y-8">{children}</div>
           </div>
